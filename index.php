19e44b4470efc59e44d791355314265d0923891f
Matt Harris <mateodelay@gmail.com>
2015-10-14 14:51:09 -0600

    Added paragraph and list

--- index.php
+++ index.php
@@ -5,5 +5,19 @@
 		</head>
 			<body>
 				<h1>The Onion: America's Finest News Source</h1>
+					<p>The Onion is a satirical news outlet that is found on the web at <a href="http://www.theonion.com" target="_blank">www.theonion.com</a>.
+						The site is composed of articles, graphics, videos, and headlines with a picture.
+						The content is categorized in different sections. The sections are as follows:</p>
+							<ul>
+								<li>Video</li>
+								<li>Politics</li>
+								<li>Sports</li>
+								<li>Local</li>
+								<li>Business</li>
+								<li>Entertainment</li>
+								<li>Science & Technology</li>
+								<li>After Birth</li>
+							</ul>
+
 			</body>
 </html>
