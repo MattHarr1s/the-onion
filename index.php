d1a4cb68d2fff2fa00ea70ff22a998e893f1b076
Matt Harris <mateodelay@gmail.com>
2015-10-14 14:35:01 -0600

    Initial commit, basic HTML frame in index.php.

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+	<html lang="eng">
+		<head><meta charset="utf-8"/>
+			<title>Data Design Project</title>
+		</head>
+			<body>
+				<h1>The Onion: America's Finest News Source</h1>
+			</body>
+</html>
