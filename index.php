9503428fd4d78bdc753969c42a75e0425e0c61b9
Matt Harris <mateodelay@gmail.com>
2015-10-14 15:11:23 -0600

    Finished ordered list.

--- index.php
+++ index.php
@@ -20,7 +20,12 @@
 							</ul>
 				<h2>Data Design Analysis</h2>
 					<p>For my data design project I will evaluate the usage scenarios for both a specific article and for a category.</p>
-				<ol><li></li><li><a href="http://www.theonion.com/section/politics/" target="_blank">Onion Politics</a> </li></ol>
+					<h3>Data Design Analysis Topics</h3>
+						<ol>
+							<li><a href="http://www.theonion.com/article/bernie-sanders-repeatedly-scolded-attempting-union-51537" target="_blank">
+									Bernie Sanders Repeatedly Scolded For Attempting To Unionize Debate Moderators</a></li>
+							<li><a href="http://www.theonion.com/section/politics/" target="_blank">Onion Politics</a></li>
+						]</ol>
 
 			</body>
 </html>
