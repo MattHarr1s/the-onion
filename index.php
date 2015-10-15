9886b433e99773a823c6e43b0ea229f2157c1f22
Matt Harris <mateodelay@gmail.com>
2015-10-15 08:47:33 -0600

    Fixed a typo

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 									<h5>Use Scenario</h5>
 									<ul><li>
 											Stefon just cracked open a fresh growler of craft session IPA. The only fitting way to relax after a long day's work creating artisanal graphics and logos
-											in the free-range design studio at the local print shop. After Stefon locks up his 1968 vintage Schwinn that he personally modified with a fixed axle, he loves drinking a locally sourced craft beer and sitting down at his 2012 MacBook Pro, where he spends his miserable life stirring up political drama onFacebook. In order to do this,
+											in the free-range design studio at the local print shop. After Stefon locks up his 1968 vintage Schwinn that he personally modified with a fixed axle, he loves drinking a locally sourced craft beer and sitting down at his 2012 MacBook Pro, where he spends his miserable life stirring up political drama on Facebook. In order to do this,
 											Stefon must:
 											<p><ol>
 												<li><span title="The Onion chills.">Open Web Browser</span></li>
